5616b2cb68a928254f0a5e9f83668c48d80224dd
Lilly <reubenh14@tutanota.com>
2023-04-30 18:05:54 +0200

    contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
-contract Contract {
-    string public greet = "Hello World!";
+contract ContractFactory is Ownable, CloneFactory {
+    address public libraryAddress;
+    ClonableContract[] public proxies;
+
+    event ContractCreated(ClonableContract proxy);
+
+    constructor(address libraryAddr) public {
+        libraryAddress = libraryAddr;
+    }
+
+    function setLibraryAddress(address libraryAddr) external onlyOwner {
+        libraryAddress = libraryAddr;
+    }
+
+    function createContract(uint256 initialData) external {
+        ClonableContract proxy = ClonableContract(createClone(libraryAddress));
+        proxy.initialize(initialData);
+
+        proxies.push(proxy);
+        emit ContractCreated(proxy);
+    }
 }
